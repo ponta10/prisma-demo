855c145a7f8bba7babae3ec585cb1074e9252879
ponta10 <mponta2002@gmail.com>
2023-06-12 17:44:09 +0900

    api setting

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+
+  if (req.method === "GET") {
+    try {
+      const post = await prisma.posts.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      return res.status(200).json(post);
+    } catch (error) {
+      return res.status(500).json({ error: "Unable to retrieve post" });
+    }
+  } else if (req.method === "PUT") {
+    const { title, body } = req.body;
+
+    try {
+      const updatedPost = await prisma.posts.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          title,
+          body,
+        },
+      });
+
+      return res.status(200).json(updatedPost);
+    } catch (error) {
+      return res.status(500).json({ error: "Unable to update post" });
+    }
+  } else if (req.method === "DELETE") {
+    try {
+      await prisma.posts.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      return res.status(204).end();
+    } catch (error) {
+      return res.status(500).json({ error: "Unable to delete post" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+}
---
+++ pages/api/posts/index.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+// POST /api/posts
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { title, body } = req.body;
+
+    if (!title || !body) {
+      return res.status(400).json({ error: "Title and body are required" });
+    }
+
+    const post = await prisma.posts.create({
+      data: {
+        title,
+        body,
+        created_at: new Date(),
+      },
+    });
+
+    return res.status(201).json(post);
+  }
+
+  if (req.method === "GET") {
+    try {
+      const posts = await prisma.posts.findMany();
+      return res.status(200).json(posts);
+    } catch (error) {
+      return res.status(500).json({ error: "Unable to retrieve posts" });
+    }
+  }
+
+  return res.status(405).end(); // Method Not Allowed
+}
